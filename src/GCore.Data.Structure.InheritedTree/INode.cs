b6ce88402ec87016a35b2a6061b0a5ad4ebf37d0
Kevin Gliewe <kevingliewe@outlook.de>
2020-02-13 16:31:58 +0100

    Add a bit of in-code documentation.

--- src/GCore.Data.Structure.InheritedTree/INode.cs
+++ src/GCore.Data.Structure.InheritedTree/INode.cs
@@ -6,47 +6,176 @@ namespace GCore.Data.Structure.InheritedTree
 {
     public interface INode<TKey, TValue> : INotifyChildrenChanged<TKey, TValue>, INotifyPropertyChanged<TKey, TValue>
     {
-
+        /// <summary>
+        /// The name of this node.
+        /// </summary>
         String Name { get; }
+
+        /// <summary>
+        /// All names from the root to this node joined with ':'.
+        /// </summary>
         String Path { get; }
+
+        /// <summary>
+        /// Distance from the root node.
+        /// </summary>
         uint Depth { get; }
 
+        /// <summary>
+        /// The parent node of this node.
+        /// Null if this node is the root node.
+        /// </summary>
         INode<TKey, TValue> Parent { get; }
 
+        /// <summary>
+        /// The tree this node belongs to.
+        /// </summary>
         ITree<TKey, TValue> Tree { get; }
 
+        /// <summary>
+        /// All propertys this node has.
+        /// Inherited or seld defined.
+        /// </summary>
         IEnumerable<IProperty<TKey, TValue>> Propertys { get; }
 
+        /// <summary>
+        /// Returns true if the property is defined by by a parent and not
+        /// by this nodes itself.
+        /// </summary>
+        /// <param name="key">The property identifier</param>
+        /// <returns></returns>
         bool IsInherted(TKey key);
+
+        /// <summary>
+        /// Returns true if this node (re)defines this property.
+        /// </summary>
+        /// <param name="key">The property identifier</param>
+        /// <returns></returns>
         bool Defines(TKey key);
 
+        /// <summary>
+        /// Returns true if this node has the defined property.
+        /// Either througth inheritance of self defining.
+        /// </summary>
+        /// <param name="key">The property identifier</param>
+        /// <returns></returns>
         bool Has(TKey key);
+
+        /// <summary>
+        /// Returns the property the node has either througth inheritance of self defining.
+        /// Returns null if the property is not denined for this node.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
         TValue Get(TKey key);
+
+        /// <summary>
+        /// (Re)Defines the property for this node.
+        /// This overrides a inherted property.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
         bool Set(TKey key, TValue value);
+
+        /// <summary>
+        /// Gets all propertys this node has.
+        /// Either througth inheritance of self defining.
+        /// </summary>
+        /// <returns></returns>
         IEnumerable<IProperty<TKey, TValue>> GetAll();
 
+        /// <summary>
+        /// Returns the property the node has either througth inheritance of self defining.
+        /// Returns null if the property is not denined for this node.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
         TValue this[TKey key]
         {
             get;
             set;
         }
 
+        /// <summary>
+        /// Returns all propertys beween the root and this nodes in order.
+        /// Ignores if the property is overritten by a child node.
+        /// </summary>
+        /// <param name="keys"></param>
+        /// <returns></returns>
         IEnumerable<IProperty<TKey, TValue>> CollectPropertys(TKey keys);
 
+        /// <summary>
+        /// Get all child nodes beneeth this node recursive.
+        /// </summary>
+        /// <param name="mexDepth">The maximal depth for the recursion</param>
+        /// <returns></returns>
         IEnumerable<INode<TKey, TValue>> GetChildren(uint mexDepth = 0);
+
+        /// <summary>
+        /// Returns all nodes from the root node to this node (inclusive) in this order.
+        /// </summary>
+        /// <returns></returns>
         IEnumerable<INode<TKey, TValue>> GetParents();
 
+        /// <summary>
+        /// Sets the parent of this node.
+        /// Throws exception if a parent is already defined.
+        /// </summary>
+        /// <param name="parent">The new parent node</param>
         void SetParent(INode<TKey, TValue> parent);
+
+        /// <summary>
+        /// Resets the parent node.
+        /// </summary>
         void RemoveParent();
 
+        /// <summary>
+        /// Gets the child node by its name.
+        /// This is NOT recursive!
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
         INode<TKey, TValue> GetChild(string name);
+
+        /// <summary>
+        /// Adds a new child to this node.
+        /// </summary>
+        /// <param name="child"></param>
         void AddChild(INode<TKey, TValue> child);
+
+        /// <summary>
+        /// Adds multible childs to this node.
+        /// </summary>
+        /// <param name="child"></param>
         void AddChildren(IEnumerable<INode<TKey, TValue>> child);
+
+        /// <summary>
+        /// Creates a new node and adds it as a child.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
         INode<TKey, TValue> CreateChild(string name);
 
+        /// <summary>
+        /// Removes a child from this node.
+        /// </summary>
+        /// <param name="child"></param>
+        /// <returns>False if it wasn't a child of this node</returns>
         bool RemoveChild(INode<TKey, TValue> child);
 
+        /// <summary>
+        /// Find a node by its relative path.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
         INode<TKey, TValue> FindNode(String path);
+
+        /// <summary>
+        /// Find a node by its relative path.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
         INode<TKey, TValue> FindNode(IEnumerable<string> path);
     }
 }
